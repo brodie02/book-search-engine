8bbef67ac212f15d6bf3b54ff18b0f1b91f108e4
brodie02 <marshallbrodie0@gmail.com>
2023-02-13 18:59:28 +1030

    Apollo not starting

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,6 @@
-import { AuthenticationError } from 'apollo-server-express'
-import { User } from '../models'
-import { signToken } from '../utils/auth'
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
@@ -22,8 +22,18 @@ const resolvers = {
             );
             return updatedUser
         },
-        removeBook: async (parent, { userId }) => {
-            return User.findOneAndDelete({ _id: userId })
+        removeBook: async (parent, { userId, bookId }) => {
+            return User.findOneAndUpdate(
+                { _id: userId },
+                {
+                    $pull: {
+                        savedBooks: {
+                            bookId: bookId
+                        }
+                    }
+                },
+                { new: true }
+            )
         },
         login: async (parent, {email, password}) => {
             const user = await User.findOne({ email })
@@ -44,3 +54,5 @@ const resolvers = {
         }
     }
 }
+
+module.exports = resolvers
